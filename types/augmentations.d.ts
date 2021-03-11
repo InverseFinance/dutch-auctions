8b8e0ecaef613777eb5d0d8fa72b9e88cc9e15b0
Jackie Xu <jackie@aeveus.com>
2021-03-11 09:45:07 +0100

    chore: remove Greeter example

--- types/augmentations.d.ts
+++ types/augmentations.d.ts
@@ -2,11 +2,9 @@
 import { Fixture } from "ethereum-waffle";
 
 import { Signers } from "./";
-import { Greeter } from "../typechain";
 
 declare module "mocha" {
   export interface Context {
-    greeter: Greeter;
     loadFixture: <T>(fixture: Fixture<T>) => Promise<T>;
     signers: Signers;
   }
