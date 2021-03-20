52a6b2d999cd1722865653b4ba80aad41eacbc7c
Jackie Xu <jackie@aeveus.com>
2021-03-20 11:46:01 +0100

    test: add additional tests for AuctionHouse and FungibleAuction

--- test/AuctionHouse.ts
+++ test/AuctionHouse.ts
@@ -4,56 +4,83 @@ import { Artifact } from "hardhat/types";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import { BigNumber } from "@ethersproject/bignumber";
 
-import { AuctionHouse } from "../typechain";
-import { MockToken } from "../typechain";
+import {
+  AuctionHouse,
+  MockToken,
+} from "../typechain";
 import { Signers } from "../types";
 
 const { deployContract } = hre.waffle;
 
-describe("Unit tests", function () {
-  before(async function () {
-    this.signers = {} as Signers;
+describe("Auction House", () => {
+  // Contract artifacts
+  let auctionArtifact: Artifact;
+  let auctionHouseArtifact: Artifact;
+  let fungibleTokenArtifact: Artifact;
 
-    const signers: SignerWithAddress[] = await hre.ethers.getSigners();
-    this.signers.admin = signers[0];
-    this.signers.auctioneer = signers[1];
-    this.signers.bidderX = signers[2];
+  // Wallet addresses.
+  const wallets = {} as Signers;
+
+  // Deployed contracts. These get refreshed per test.
+  let auctionHouse: AuctionHouse;
+  let mockTokenA: MockToken;
+  let mockTokenB: MockToken;
+
+  // Shared test constants
+  const oneToken = BigNumber.from("1000000000000000000");
+  const tokensToTransfer: BigNumber = oneToken.mul("2000"); // two thousand
+  const tokensToAuction: BigNumber = oneToken.mul("1000"); // one thousand
+  const maxPrice: BigNumber = BigNumber.from("10"); // ten
+  const minPrice: BigNumber = BigNumber.from("5"); // five
+
+
+  before(async () => {
+    let signers: SignerWithAddress[] = await hre.ethers.getSigners();
+
+    wallets.admin = signers[0];
+    wallets.auctioneer = signers[1];
+    wallets.bidderX = signers[2];
+
+    auctionArtifact = await hre.artifacts.readArtifact("FungibleAuction");
+    auctionHouseArtifact = await hre.artifacts.readArtifact("AuctionHouse");
+    fungibleTokenArtifact = await hre.artifacts.readArtifact('MockToken');
   });
 
-  describe("AuctionHouse", function () {
-    beforeEach(async function () {
-      const auctionHouseArtifact: Artifact = await hre.artifacts.readArtifact("AuctionHouse");
-      const erc20Artifact: Artifact = await hre.artifacts.readArtifact('MockToken');
-      const tokenCount: BigNumber = BigNumber.from("1000000000000000000000000"); // one million
+  beforeEach(async () => {
+    const mockTokenASupply: BigNumber = oneToken.mul(BigNumber.from(1e5));
+    const mockTokenBSupply: BigNumber = oneToken.mul(BigNumber.from(1e8));
 
-      this.auctionHouse = <AuctionHouse>await deployContract(this.signers.admin, auctionHouseArtifact, []);
-      this.tokenA = <MockToken>await deployContract(this.signers.admin, erc20Artifact, ["Test Token A", "TTA", tokenCount]);
-      this.tokenB = <MockToken>await deployContract(this.signers.admin, erc20Artifact, ["Test Token B", "TTB", tokenCount]);
+    auctionHouse = <AuctionHouse>await deployContract(wallets.admin, auctionHouseArtifact, []);
+    mockTokenA = <MockToken>await deployContract(
+      wallets.admin,
+      fungibleTokenArtifact,
+      ["Mock Inverse DAO", "MINV", mockTokenASupply],
+    )
+    mockTokenB = <MockToken>await deployContract(
+      wallets.admin,
+      fungibleTokenArtifact,
+      ["Mock LP tokens", "MLP", mockTokenBSupply],
+    );
+  });
+
+  describe("Deployments",  () => {
+    it('should have zero active auctions at creation', async () => {
+      expect(await auctionHouse.getActiveAuctionCount()).to.equal(0);
     });
 
-    it('should have zero active auctions at creation', async function () {
-      expect(await this.auctionHouse.connect(this.signers.admin).getActiveAuctionCount()).to.equal(0);
-    });
-
-    it('should accept new auctions', async function () {
-      const oneToken = BigNumber.from("1000000000000000000");
-      const tokensToTransfer: BigNumber = oneToken.mul("2000"); // two thousand
-      const tokensToAuction: BigNumber = oneToken.mul("1000"); // one thousand
-      const maxPrice: BigNumber = oneToken.mul("10"); // ten
-      const minPrice: BigNumber = oneToken.mul("5"); // five
-
+    it('should accept new auctions', async () => {
       // Send tokens to auctioneer
-      await this.tokenA.connect(this.signers.admin).transfer(this.signers.auctioneer.getAddress(), tokensToTransfer);
-      expect(await this.tokenA.connect(this.signers.auctioneer).balanceOf(this.signers.auctioneer.getAddress())).to.equal(tokensToTransfer);
+      await mockTokenA.connect(wallets.admin).transfer(wallets.auctioneer.address, tokensToTransfer);
+      expect(await mockTokenA.balanceOf(wallets.auctioneer.address)).to.equal(tokensToTransfer);
 
       // Approve token transfer
-      await this.tokenA.connect(this.signers.auctioneer).approve(this.auctionHouse.address, tokensToTransfer);
+      await mockTokenA.connect(wallets.auctioneer).approve(auctionHouse.address, tokensToTransfer);
 
       // Create auction
       const currentTimestamp: number = Math.floor(Date.now() / 1000);
-      await this.auctionHouse.connect(this.signers.auctioneer).createFungible(
-        this.tokenA.address,
-        this.tokenB.address,
+      await auctionHouse.connect(wallets.auctioneer).createFungible(
+        mockTokenA.address,
+        mockTokenB.address,
         tokensToAuction,
         BigNumber.from(currentTimestamp),
         BigNumber.from("600"),
@@ -61,94 +88,39 @@ describe("Unit tests", function () {
         minPrice,
       );
 
-      expect(await this.auctionHouse.connect(this.signers.admin).getActiveAuctionCount()).to.equal(1);
+      expect(await auctionHouse.getActiveAuctionCount()).to.equal(1);
+      expect(await auctionHouse.getFungibleAuction(0)).to.not.equal('0x0000000000000000000000000000000000000000');
     });
 
-    it('should allow bid to buy everything', async function () {
-      const oneToken = BigNumber.from("1000000000000000000");
-      const tokensToTransfer: BigNumber = oneToken.mul("2000"); // two thousand
-      const tokensToAuction: BigNumber = oneToken.mul("1000"); // one thousand
-      const maxPrice: BigNumber = BigNumber.from("10"); // ten
-      const minPrice: BigNumber = BigNumber.from("5"); // five
-      const tokensToBid: BigNumber = tokensToAuction.mul(maxPrice);
-
-      // Send tokens to auctioneer
-      await this.tokenA.connect(this.signers.admin).transfer(this.signers.auctioneer.getAddress(), tokensToTransfer);
-
-      // Approve token transfer
-      await this.tokenA.connect(this.signers.auctioneer).approve(this.auctionHouse.address, tokensToAuction);
-
-      // Create auction
+    it('should revert if auctioneer has insufficient funds', async () => {
       const currentTimestamp: number = Math.floor(Date.now() / 1000);
-      await this.auctionHouse.connect(this.signers.auctioneer).createFungible(
-        this.tokenA.address,
-        this.tokenB.address,
+      await expect(auctionHouse.connect(wallets.auctioneer).createFungible(
+        mockTokenA.address,
+        mockTokenB.address,
         tokensToAuction,
         BigNumber.from(currentTimestamp),
         BigNumber.from("600"),
         maxPrice,
         minPrice,
-      );
-
-      // Send tokens to bidder
-      await this.tokenB.connect(this.signers.admin).transfer(this.signers.bidderX.getAddress(), tokensToBid);
-
-      // Approve bid tokens
-      await this.tokenB.connect(this.signers.bidderX).approve(this.auctionHouse.getFungibleAuction(0), tokensToBid);
-
-      const auctionContract = await hre.ethers.getContractAt('FungibleAuction', this.auctionHouse.getFungibleAuction(0));
-      await auctionContract.connect(this.signers.bidderX).buy(tokensToAuction);
-
-      // Should have received the requested number of tokens
-      expect(await this.tokenA.connect(this.signers.bidderX).balanceOf(this.signers.bidderX.getAddress())).to.equal(tokensToAuction);
-
-      // Should have paid in between the maxPrice and minPrice range
-      expect(await this.tokenB.connect(this.signers.bidderX).balanceOf(this.signers.bidderX.getAddress())).to.lte(tokensToBid.sub(tokensToAuction.mul(minPrice)));
-      expect(await this.tokenB.connect(this.signers.bidderX).balanceOf(this.signers.bidderX.getAddress())).to.gte(tokensToBid.sub(tokensToAuction.mul(maxPrice)));
+      )).to.be.revertedWith("AuctionHouse: Creator not liquid enough");
     });
 
-    it('should allow withdrawal after completion', async function () {
-      const oneToken = BigNumber.from("1000000000000000000");
-      const tokensToTransfer: BigNumber = oneToken.mul("2000"); // two thousand
-      const tokensToAuction: BigNumber = oneToken.mul("1000"); // one thousand
-      const maxPrice: BigNumber = BigNumber.from("10"); // ten
-      const minPrice: BigNumber = BigNumber.from("5"); // five
-      const tokensToBid: BigNumber = tokensToAuction.mul(maxPrice);
-
+    it('should revert if auctioneer has not approved', async () => {
       // Send tokens to auctioneer
-      await this.tokenA.connect(this.signers.admin).transfer(this.signers.auctioneer.getAddress(), tokensToTransfer);
+      await mockTokenA.connect(wallets.admin).transfer(wallets.auctioneer.address, tokensToTransfer);
+      expect(await mockTokenA.balanceOf(wallets.auctioneer.address)).to.equal(tokensToTransfer);
 
-      // Approve token transfer
-      await this.tokenA.connect(this.signers.auctioneer).approve(this.auctionHouse.address, tokensToAuction);
-
-      // Create auction
       const currentTimestamp: number = Math.floor(Date.now() / 1000);
-      await this.auctionHouse.connect(this.signers.auctioneer).createFungible(
-        this.tokenA.address,
-        this.tokenB.address,
+
+      await expect(auctionHouse.connect(wallets.auctioneer).createFungible(
+        mockTokenA.address,
+        mockTokenB.address,
         tokensToAuction,
         BigNumber.from(currentTimestamp),
         BigNumber.from("600"),
         maxPrice,
         minPrice,
-      );
-
-      // Send tokens to bidder
-      await this.tokenB.connect(this.signers.admin).transfer(this.signers.bidderX.getAddress(), tokensToBid);
-
-      // Approve bid tokens
-      await this.tokenB.connect(this.signers.bidderX).approve(this.auctionHouse.getFungibleAuction(0), tokensToBid);
-
-      const auctionContract = await hre.ethers.getContractAt('FungibleAuction', this.auctionHouse.getFungibleAuction(0));
-      await auctionContract.connect(this.signers.bidderX).buy(tokensToAuction);
-
-      // Withdraw tokens
-      const expectedBalance = tokensToBid.sub(await this.tokenB.balanceOf(this.signers.bidderX.getAddress()));
-      await auctionContract.connect(this.signers.auctioneer).withdraw();
-      expect(await this.tokenB.balanceOf(this.signers.auctioneer.getAddress())).to.equal(expectedBalance);
-
-      // Number of active auction should be zero
-      expect(await this.auctionHouse.getActiveAuctionCount()).to.equal(0);
+      )).to.be.revertedWith("AuctionHouse: Not approved");
     });
   });
 });
